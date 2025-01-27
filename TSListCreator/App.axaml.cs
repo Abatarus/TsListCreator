3ef57bbb380f6a437fe9fe630221862945eda838
Abatarus <giss1302@yandex.ru>
2025-01-27 06:03:14 +0300

    feat: loading image

--- TSListCreator/App.axaml.cs
+++ TSListCreator/App.axaml.cs
@@ -17,7 +17,7 @@ namespace TSListCreator
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
             {
                 desktop.MainWindow = new MainWindow();
-                desktop.MainWindow.DataContext = new MainViewModel();
+                desktop.MainWindow.DataContext = new MainViewModel(desktop.MainWindow);
             }
 
             base.OnFrameworkInitializationCompleted();
--- TSListCreator/ViewModels/MainViewModel.cs
+++ TSListCreator/ViewModels/MainViewModel.cs
@@ -1,4 +1,8 @@
+using System.IO;
+using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Media.Imaging;
+using Avalonia.Platform.Storage;
 using TSListCreator.Utils;
 
 namespace TSListCreator.ViewModels;
@@ -9,13 +13,36 @@ public class MainViewModel : DataModel
     public Bitmap? Image
     {
         get => _image;
-        set => SetField(ref _image, value);
+        set
+        {
+            SetField(ref _image, value);
+            OnPropertyChanged(nameof(CanInteract));
+        }
     }
 
-    public bool CanInteract
+    public bool CanInteract => Image != null;
+
+    private object _view;
+    public MainViewModel(object view)
     {
-        get => Image != null;
+        _view = view;
     }
-    public MainViewModel() {
+
+    public async void LoadImage()
+    {
+        var topLevel = TopLevel.GetTopLevel((Visual)_view);
+        var value = new Avalonia.Platform.Storage.FilePickerOpenOptions
+        {
+            Title = "Выберете изображение",
+            AllowMultiple = false,
+            FileTypeFilter = new[] { FilePickerFileTypes.ImagePng, FilePickerFileTypes.ImageAll }
+        };
+        var files = await topLevel!.StorageProvider.OpenFilePickerAsync(value);
+
+        if (files.Count >= 1)
+        {
+            await using Stream stream = await files[0].OpenReadAsync();
+            Image = new Bitmap(stream);
+        }
     }
 }
