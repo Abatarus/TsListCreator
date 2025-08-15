bef4e48140215ba45212d1b07a2f08821787fc1e
Abatarus <giss1302@yandex.ru>
2025-08-15 14:07:57 +0300

    feat: Добавлен проект для тестов

---
+++ TSListCreator.Tests/UI/MainWindowTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Avalonia.Headless.XUnit;
+using Avalonia.Threading;
+using TSListCreator.Services;
+using TSListCreator.ViewModels;
+using TSListCreator.Views;
+
+namespace TSListCreator.Tests.UI
+{
+    public class MainWindowTest
+    {
+        [AvaloniaFact]
+        public void MainWindow_Open()
+        {
+            var record = Record.Exception(() =>
+            {
+                var window = new MainWindow();
+                var view = new MainView();
+                ImageDataService imageDataService = new ImageDataService();
+                SingletonServiceContainer serviceContainer = new SingletonServiceContainer(imageDataService);
+                LoadImageService loadImageService = new LoadImageService(window);
+                var viewModel = new MainViewModel(loadImageService, serviceContainer.ImageDataService);
+                view.DataContext = viewModel;
+
+                window.Show();
+                Dispatcher.UIThread.RunJobs();
+            });
+            Assert.Null(record);
+        }
+    }
+}
