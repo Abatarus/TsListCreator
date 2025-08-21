60f232ed73e88ec58c4eb5c0ec71d7a88b4cd669
Abatarus <giss1302@yandex.ru>
2025-08-21 22:53:25 +0300

    feat: тесты для конвертера в позицию в игре

---
+++ TSListCreator.Tests/Converters/CanvasCoorToTsPosConverterTest.cs
@@ -0,0 +1,33 @@
+﻿using Avalonia;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TSListCreator.Controls;
+using TSListCreator.Converters;
+using TSListCreator.Services;
+using TSListCreator.Tests.Mocks;
+
+namespace TSListCreator.Tests.Converters
+{
+    public class CanvasCoorToTsPosConverterTest
+    {
+        [Fact]
+        public void Convert_ValueIsZero_ShouldReturnAsInTableTop()
+        {
+            SingletonServiceContainer serviceContainer =
+                new SingletonServiceContainer(new ImageDataServiceMock().FakedObject);
+            var converter = new CanvasCoorToTsPosConverter();
+
+            var height = 0.0;
+            double expected = -1.9 ;
+
+            double result = (double)converter.Convert(height, null, "Height", CultureInfo.CurrentCulture);
+
+            Assert.Equal(expected, result, 1);
+        }
+    }
+}
---
+++ TSListCreator.Tests/Mocks/ImageDataServiceMock.cs
@@ -0,0 +1,23 @@
+﻿using FakeItEasy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TSListCreator.Services;
+
+namespace TSListCreator.Tests.Mocks
+{
+    class ImageDataServiceMock : Fake<ImageDataService>
+    {
+        public ImageDataServiceMock()
+        {
+            CallsTo(x =>
+                    x.GetImagePixHeight())
+                .Returns(500);
+            CallsTo(x =>
+                    x.GetImageHeight())
+                .Returns(1000);
+        }
+    }
+}
